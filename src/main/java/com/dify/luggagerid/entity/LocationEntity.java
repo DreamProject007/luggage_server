8d7dae17468f0ef96d404fda9ad815fe085df053
DreamProject007 <77226198+DreamProject007@users.noreply.github.com>
2023-07-01 23:02:34 +0530

    config changes

--- src/main/java/com/dify/luggagerid/entity/LocationEntity.java
+++ src/main/java/com/dify/luggagerid/entity/LocationEntity.java
@@ -1,6 +1,8 @@
 package com.dify.luggagerid.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,41 +11,70 @@ import javax.persistence.Table;
 public class LocationEntity {
 
 	@Id
-	public String coordinate;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	public Integer id;
+	public Double latitude;
+	public Double longitude;
 	public String city;
 	public String place;
 	public Integer rating;
 	public String description;
-	
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
 	public Integer getRating() {
 		return rating;
 	}
+
 	public void setRating(Integer rating) {
 		this.rating = rating;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getPlace() {
 		return place;
 	}
+
 	public void setPlace(String place) {
 		this.place = place;
 	}
-	public String getCoordinate() {
-		return coordinate;
-	}
-	public void setCoordinate(String coordinate) {
-		this.coordinate = coordinate;
+
+	public Integer getId() {
+		return id;
 	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	// override toString method
 }
